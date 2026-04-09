752a53f4927615667e6ea20e8e099ea188121527
VivekKumarNetcarrots <vivek.netcarrots@gmail.com>
2026-04-09 15:51:29 +0530

    www

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,23 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
-
+  deferredPrompt: any;
+  constructor(private swUpdate: SwUpdate) {}
   ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event: any) => {
+        if (event.type === 'VERSION_READY') {
+          this.showUpdatePopup();
+        }
+      });
+    }
+    if (this.isStandalone()) {
+      console.log('Running as installed app ✅');
+    } else {
+      console.log('Running in browser 🌐');
+      this.checkInstall();
+    }
+
     setTimeout(() => {
       const splash = document.getElementById('splash-screen');
       if (splash) {
@@ -18,5 +33,48 @@ export class AppComponent implements OnInit {
         setTimeout(() => splash.remove(), 500);
       }
     }, 500); // small delay for smoother UX
+  }
+  showUpdatePopup() {
+    const updateBox = document.getElementById('update-box');
+    if (updateBox) updateBox.style.display = 'flex';
+  }
+
+  updateApp() {
+    window.location.reload();
+  }
+  isStandalone(): boolean {
+    return (
+      window.matchMedia('(display-mode: standalone)').matches ||
+      (window.navigator as any).standalone === true
+    );
+  }
+
+  checkInstall() {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+      // show your custom banner
+      const banner = document.getElementById('install-banner');
+      if (banner) banner.style.display = 'flex';
+    });
+  }
+
+  installApp() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then(() => {
+        this.deferredPrompt = null;
+        this.hideBanner();
+      });
+    }
+  }
+
+  dismissBanner() {
+    this.hideBanner();
+  }
+
+  hideBanner() {
+    const banner = document.getElementById('install-banner');
+    if (banner) banner.style.display = 'none';
   }
 }
