42c69d9f3c3256635311ac9be4550daa625b5f2d
VivekKumarNetcarrots <vivek.netcarrots@gmail.com>
2026-04-10 16:53:36 +0530

    updated

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-} from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 import { TicTacComponent } from '../comps/tic-tac/tic-tac.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, TicTacComponent],
+  imports: [IonContent, TicTacComponent],
 })
 export class Tab1Page {
   constructor() {}
