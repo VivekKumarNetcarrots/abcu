005ffdf31f5109d66469df957d174bbb875f2f4f
VivekKumarNetcarrots <vivek.netcarrots@gmail.com>
2026-08-23 20:26:26 +0530

    23 Aug 26

--- src/app/pages/games/games.page.ts
+++ src/app/pages/games/games.page.ts
@@ -7,11 +7,17 @@ import {
   IonButtons,
   IonButton,
   IonIcon,
+  IonMenu,
+  IonMenuButton,
+  MenuController,
 } from '@ionic/angular/standalone';
 import { GamesContainerComponent } from 'src/app/comps/games-container/games-container.component';
 import { addIcons } from 'ionicons';
-import { flowerOutline, menu } from 'ionicons/icons';
+import { flowerOutline, home, menu } from 'ionicons/icons';
 import { CommonModule } from '@angular/common';
+import { ContactComponent } from 'src/app/comps/contact/contact.component';
+import { PrivacyComponent } from 'src/app/comps/privacy/privacy.component';
+import { AboutComponent } from 'src/app/comps/about/about.component';
 
 @Component({
   selector: 'app-games',
@@ -28,16 +34,51 @@ import { CommonModule } from '@angular/common';
     IonButtons,
     IonButton,
     IonIcon,
+    IonMenu,
+    IonMenuButton,
   ],
 })
 export class GamesPage {
   selectedGame: any;
-  constructor() {
-    addIcons({ menu });
+
+  gamesList = [
+    {
+      name: 'Contact Us',
+      desc: 'Contact Us 24x7.',
+      img: '/assets/gifs/contact.gif',
+      url: '/contact',
+      component: ContactComponent,
+      id: 3,
+    },
+    {
+      name: 'Privacy Policy',
+      desc: 'Know Privacy Policy.',
+      img: '/assets/gifs/privacy.gif',
+      url: '/privacy',
+      component: PrivacyComponent,
+      id: 4,
+    },
+    {
+      name: 'About US',
+      desc: 'Know about us.',
+      img: '/assets/gifs/about.gif',
+      url: '/about',
+      component: AboutComponent,
+      id: 5,
+    },
+  ];
+
+  constructor(private menuC: MenuController) {
+    addIcons({ menu, home });
   }
 
   onGameSelected(event: any) {
     this.selectedGame = event;
     console.log('OnGameSelected:>>>', event);
+  }
+  onMenuSelect(g: any) {
+    this.selectedGame = g.component;
+    this.menuC.close();
+    // this.navC.navigateForward(g.url);
   }
 }
